2d4a64c8bdc3b2d9a1459f3e1c0c8ca45ba212aa
Koushik Raghav Sankar <raghavan.sankar@gmail.com>
2019-07-17 14:24:39 +0530

    Add files via upload

---
+++ averaging.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+image = cv2.imread('red_eyes2.jpg')
+blur = median = cv2.blur(image,(5,5))
+plt.subplot(121),plt.imshow(image),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(blur),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.show()
---
+++ gaussianFilter.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+image = cv2.imread('red_eyes2.jpg')
+blur = median = cv2.GaussianBlur(img,(5,5),0)
+plt.subplot(121),plt.imshow(image),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(blur),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.show()
